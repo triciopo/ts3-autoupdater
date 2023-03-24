22d905cd6b5c85be740ac7158b7e26f7a937285a
thiagofrere <thiagoriciopo@gmail.com>
2023-03-24 16:02:04 -0300

    Add launch options

--- update.sh
+++ update.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env bash
+#
+# ts3-autoupdater - A simple bash script to auto-update a TeamSpeak Server.
+#
+# Usage: ./update.sh [-d SERVER_DIR] [-b BACKUP_DIR]
+#
+# Options:
+# -d SERVER_DIR -> Specify the server directory. Default: script's directory.
+# -b BACKUP_DIR  -> Specify the backup folder. Default: script's directory/backups.
+#
+
 [[ -v debug ]] && set -x
+
+server_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+backup_dir="$server_dir/backups"
 
 trap \
   "{ cleanup ;}" \
   SIGINT SIGTERM ERR EXIT
 
+#---- Delete temporary dl dir (if it exists)
 function cleanup() { if [[ -d $tmpdir ]]; then rm -rf "$tmpdir"; fi; }
 
 #---- Get newest ts3 server download url 
@@ -19,39 +33,59 @@ function getURL() {
 
 #---- Check for updates
 function check() {
-  readonly version=$(curl -Ls 'https://www.teamspeak.com/versions/server.json' | jq -r 'first(.[] | .[] | .version)')
-  case $(grep -F "$version" "CHANGELOG" >/dev/null; echo $?) in
-    0)
-      printf "[%s] Server already running on the latest version ($version).\n" "$(date +%c)"
-      ;;
-  	1)
-      printf "[%s] New version $version found. Updating...\n" "$(date +%c)"
-	    getURL
-      ;;
-    *)
-	    exit 1
-      ;;
-  esac
+  cd "$server_dir" || exit 1
+  version=$(curl -Ls 'https://www.teamspeak.com/versions/server.json' | jq -r 'first(.[] | .[] | .version)')
+  readonly version
+  if [[ -f "CHANGELOG" ]]; then
+    case $(grep -F "$version" "CHANGELOG" >/dev/null; echo $?) in
+      0)
+        printf "[%s] Server already running on the latest version ($version).\n" "$(date +%c)" 
+        ;;
+      1)
+        printf "[%s] New version $version found. Updating...\n" "$(date +%c)"
+    	  getURL
+        ;;
+      *)
+    	  exit 1 ;;
+      esac
+  else
+    printf "[%s] Could not find current server version.\n" "$(date +%c)" >&2
+    exit 1
+  fi
 }
 
 #---- Update ts3 server
 function update() {
   if pgrep -f ts3server &> /dev/null 2>&1; then
     printf "[%s] Stopping TS3 Server...\n" "$(date +%c)"
-    ./ts3server_startscript.sh stop
+    pkill -f ts3server
   fi
-  if [[ ! -d "backups" ]]; then
-    mkdir backups
+  if [[ ! -d $backup_dir ]]; then
+    printf "[%s] Creating backup dir $OPTARG\n" "$(date +%c)"
+    mkdir "$backup_dir" || exit 1
   fi
-  #Backup folder
-  tar --exclude='backups' --overwrite -cf backups/"$(date +%F)".tar -- *
+  #Backup old server
+  tar --exclude='backups' --overwrite -cf "$backup_dir"/"$(date +%F)".tar -- *
   # Download and install new update
   tmpdir=$(mktemp -d /tmp/ts3auto.XXXXX)
   curl -Ls "$url" -o "$tmpdir"/ts3.tar.bz2 
   tar -xf "$tmpdir"/ts3.tar.bz2 -C "$tmpdir"
   cp -a "$tmpdir"/teamspeak3-server_linux_amd64/. .
-  printf "[%s] Update complete. Backup saved on: $(pwd)/backup\n" "$(date +%c)"
+  ./ts3server_startscript.sh start
+  printf "[%s] Update complete. Backup saved on: $backup_dir\n" "$(date +%c)"
 }
 
-cd "$(dirname "$0")" || exit
+# Options
+while getopts ":d:b:" opt; do
+  case $opt in
+    d)
+      server_dir=$(realpath "$OPTARG")
+      if [[ ! -d $server_dir ]]; then
+        printf "[%s] Could not find working folder.\n" "$(date +%c)" >&2 && exit 1 
+      fi ;;
+    b) backup_dir="$OPTARG" ;;
+    *) printf "[%s] Invalid argument -%s\n" "$(date +%c)" "$OPTARG" >&2 && exit 1 ;;
+  esac
+done
+
 check
