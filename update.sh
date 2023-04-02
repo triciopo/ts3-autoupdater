8f847f8a67726ee34c142402896d43a78629adc3
thiagofrere <thiagoriciopo@gmail.com>
2023-04-02 01:38:07 -0300

    Improve version detection & change backup dir

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#
+#===================================================================================
 # ts3-autoupdater - A simple bash script to auto-update a TeamSpeak Server.
 #
 # Usage: ./update.sh [-h] [-d SERVER_DIR] [-b BACKUP_DIR]
@@ -7,9 +7,11 @@
 # Options:
 # -h, --help -> Show help message and exit.
 # -d, --directory SERVER_DIR -> Specify the server directory. Default: script's directory.
-# -b, --backup BACKUP_DIR  -> Specify the backup folder. Default: script's directory/backups.
+# -b, --backup BACKUP_DIR  -> Specify the backup folder. Default: SERVER_DIR/backups.
+#===================================================================================
 
 [[ -v debug ]] && set -x
+set -e
 
 server_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 backup_dir="$server_dir/backups"
@@ -22,32 +24,30 @@ trap \
 function cleanup() { if [[ -d $tmpdir ]]; then rm -rf "$tmpdir"; fi; }
 
 #---- Get newest ts3 server download url 
-function getURL() {
+function get_url() {
   if uname -m | grep '64'; then
-    url="https://files.teamspeak-services.com/releases/server/$version/teamspeak3-server_linux_amd64-$version.tar.bz2"
+    url="https://files.teamspeak-services.com/releases/server/$latest_version/teamspeak3-server_linux_amd64-$latest_version.tar.bz2"
   else
-    url="https://files.teamspeak-services.com/releases/server/$version/teamspeak3-server_linux_x86-$version.tar.bz2"
+    url="https://files.teamspeak-services.com/releases/server/$latest_version/teamspeak3-server_linux_x86-$latest_version.tar.bz2"
   fi
-    update
+  update
 }
 
 #---- Check for updates
 function check() {
   cd "$server_dir" || exit 1
-  version=$(curl -Ls 'https://www.teamspeak.com/versions/server.json' | jq -r 'first(.[] | .[] | .version)')
-  readonly version
-  if [[ -f "CHANGELOG" ]]; then
-    case $(grep -F "$version" "CHANGELOG" >/dev/null; echo $?) in
-      0)
-        printf "[%s] Server already running on the latest version ($version).\n" "$(date +%c)" 
-        ;;
-      1)
-        printf "[%s] New version $version found. Updating...\n" "$(date +%c)"
-    	getURL
-        ;;
-      *)
-        exit 1 ;;
-      esac
+  latest_version=$(curl -Ls 'https://www.teamspeak.com/versions/server.json' | jq -r 'first(.[] | .[] | .version)')
+  readonly latest_version
+  if [[ -f "ts3server" ]]; then
+    current_version=$(./ts3server | sed -n 's/.*TeamSpeak 3 Server \([0-9.]\+\).*/\1/p;q')
+    readonly current_version
+    if [[ $current_version = "$latest_version" ]]; then
+      printf "[%s] Server already running on the latest version ($latest_version).\n" "$(date +%c)"
+      exit 0
+    else
+      printf "[%s] New version $latest_version found. Updating...\n" "$(date +%c)"
+      get_url
+    fi
   else
     printf "[%s] ERROR: Could not find current server version.\n" "$(date +%c)" >&2
     exit 1
@@ -59,6 +59,9 @@ function update() {
   if pgrep -f ts3server &> /dev/null 2>&1; then
     printf "[%s] Stopping TS3 Server...\n" "$(date +%c)"
     pkill -f ts3server
+  fi
+  if [[ "$backp" = false ]]; then
+    backup_dir="$server_dir/backups"
   fi
   if [[ ! -d $backup_dir ]]; then
     printf "[%s] Creating backup dir $OPTARG\n" "$(date +%c)"
@@ -88,6 +91,7 @@ EOF
 }
 
 #---- Parse arguments
+backp=false
 while [[ $# -gt 0 ]]; do
   case "$1" in
     -h|--help)
@@ -114,6 +118,7 @@ while [[ $# -gt 0 ]]; do
         exit 1
       fi
       backup_dir="$2"
+      backp=true
       shift
       ;;
     *)
